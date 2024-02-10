f59afff849d51587642de99104c698933452578a
Mohd Shadab <89182232+shadabibrahim@users.noreply.github.com>
2024-02-11 01:37:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+// //your JS code here. If required.
+let value = confirm("Do you want to proceed?");
+// console.log(value)
+if(value)
+	alert("You clicked OK. Proceeding...");
+else
+	alert("You clicked Cancel. Exiting...");
+
